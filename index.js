c304f98a0231f0f0d12c64f91d880fa256b7a54b
CJWeiland <45521882+CJWeiland@users.noreply.github.com>
2018-12-10 17:21:26 -0500

    Update and rename bot.js to index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({disableEveryone: true});
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
 
-bot.on('ready', async () => {
-    console.log('${bot.user.username} is online!');
-    bot.user.setActivity(`Hello there!`);
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity(`Hey, Wassup!`);
 });
 
-bot.on('message', async message => {
-    
+bot.on("message", async message => {
+
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
-    
-  let prefix = '$';
+
+  let prefix = '-';
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-  
+
   if (cmd === `${prefix}ping`){
     message.channel.send("Pong!");
   }
